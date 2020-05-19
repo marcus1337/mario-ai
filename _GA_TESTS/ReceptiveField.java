3a20daefe6fb0f0ca2efabf9426ef188f76513db
marcus1337 <blurgas@hotmail.com>
2020-05-19 16:07:54 +0200

    obstacle ahead check

---
+++ _GA_TESTS/ReceptiveField.java
@@ -0,0 +1,102 @@
+package _GA_TESTS;
+
+import engine.core.MarioForwardModel;
+
+public class ReceptiveField {
+	private int[][] observations = null;
+	public final int lenX = 5;
+	public final int lenY = 6;
+	private final int midX = 16;
+	private final int midY = 8;
+	
+	private int[][] getRightReceptiveField(){
+		int[][] field = new int[lenX][lenY];
+		int xBegin = midX;
+		int yBegin = midY - (int) Math.ceil((float) lenY / 2);
+		for (int y = yBegin; y < yBegin + lenY; y++) {
+			for (int x = xBegin; x < xBegin + lenX; x++) {
+				field[x - xBegin][y - yBegin] = observations[x][y];
+			}
+		}
+		return field;
+	}
+	private int[][] getLefttReceptiveField(){
+		int[][] field = new int[lenX][lenY];
+		int xBegin = midX;
+		int yBegin = midY - (int) Math.ceil((float) lenY / 2);
+		for (int y = yBegin; y < yBegin + lenY; y++) {
+			for (int x = xBegin-lenX+1; x <= xBegin; x++) {
+				field[x - xBegin+lenX-1][y - yBegin] = observations[x][y];
+			}
+		}
+		mirrorReceptiveField(field);
+		return field;
+	}
+	
+	private void swapArrayElems(int[][] arr, int i, int j, int y){
+		int tmp = arr[i][y];
+		arr[i][y] = arr[j][y];
+		arr[j][y] = tmp;
+	}
+	
+	private void mirrorReceptiveField(int[][] field){
+		for(int y = 0; y < lenY; y++){
+			int swapDist = lenX-1;
+			for(int x= 0; x < lenX/2; x++){
+				swapArrayElems(field,x,x+swapDist,y);
+				swapDist-=2;
+			}
+		}
+	}
+
+	public int[][] getReceptiveField(MarioForwardModel model) {					
+		observations = model.getMarioCompleteObservation(0, 0);
+		int[][] field = null;
+		if(model.isFacingRight())
+			field = getRightReceptiveField();
+		else
+			field = getLefttReceptiveField();
+		return field;
+	}
+	
+	public boolean tunneledFieldContains(int[][] field, int itemValue){
+		for(int x = 0; x < lenX; x++){
+			for(int y = 2; y < lenY-2; y++){
+				if(field[x][y] == itemValue)
+					return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean levelledFieldContains(int[][] field, int itemValue){
+		for(int x = 0; x < lenX; x++){
+			for(int y = 0; y < lenY-2; y++){
+				if(field[x][y] == itemValue)
+					return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean fieldContains(int[][] field, int itemValue){
+		for(int x = 0; x < lenX; x++){
+			for(int y = 0; y < lenY; y++){
+				if(field[x][y] == itemValue)
+					return true;
+			}
+		}
+		return false;
+	}
+	
+	public void printReceptiveField(int[][] field){
+		System.out.println("-------------------");
+		for(int y = 0; y < lenY; y++){
+			for(int x = 0; x < lenX; x++){
+				System.out.print(field[x][y] + ",");
+			}
+			System.out.println();
+		}
+		System.out.println("-------------------");
+	}
+}
--- agents/human/Agent.java
+++ agents/human/Agent.java
@@ -2,7 +2,10 @@ package agents.human;
 
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
 
+import _GA_TESTS.ReceptiveField;
+import agents.BT.BT.STATE;
 import engine.core.MarioAgent;
 import engine.core.MarioForwardModel;
 import engine.core.MarioTimer;
@@ -10,117 +13,46 @@ import engine.helper.MarioActions;
 
 public class Agent extends KeyAdapter implements MarioAgent {
 	private boolean[] actions = null;
-	private final int recLenX = 5;
-	private final int recLenY = 6;
-	private final int midX = 16;
-	private final int midY = 8;
 
 	@Override
 	public void initialize(MarioForwardModel model, MarioTimer timer) {
 		actions = new boolean[MarioActions.numberOfActions()];
 	}
 
-	private int[] getMarioBoundedPos(MarioForwardModel model) {
-		int[][] observ = model.getMarioSceneObservation();
-		int maxXPos = observ.length;
-		int maxYPos = observ[0].length;
-		int[] pos = model.getMarioScreenTilePos();
-		pos[0] = Math.max(0, pos[0]);
-		pos[0] = Math.min(pos[0], maxXPos - 1);
-		pos[1] = Math.max(0, pos[1]);
-		pos[1] = Math.min(pos[1], maxYPos - 1);
-		return pos;
+	ReceptiveField recField = new ReceptiveField();
+	int[][] field = null;
+	public MarioForwardModel model = null;
+
+	ArrayList<Integer> getObstacleValues() {
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		result.add(MarioForwardModel.OBS_BRICK);
+		result.add(MarioForwardModel.OBS_PIPE);
+		result.add(MarioForwardModel.OBS_PIPE_BODY_LEFT);
+		result.add(MarioForwardModel.OBS_PIPE_BODY_RIGHT);
+		result.add(MarioForwardModel.OBS_PIPE_TOP_LEFT);
+		result.add(MarioForwardModel.OBS_PIPE_TOP_RIGHT);
+		result.add(MarioForwardModel.OBS_CANNON);
+		result.add(MarioForwardModel.OBS_SOLID);
+		result.add(MarioForwardModel.OBS_PYRAMID_SOLID);
+		return result;
 	}
 
-	/*
-	 * int mapBoolToInt(boolean val){ if(val) return 1; return 0; }
-	 */
+	ArrayList<Integer> obstacleValues = getObstacleValues();
 
-	int[][] observations = null;
-	int[][] receptiveField = null;
-	
-	private int[][] getRightReceptiveField(){
-		int[][] field = new int[recLenX][recLenY];
-		int xBegin = midX;
-		int yBegin = midY - (int) Math.ceil((float) recLenY / 2);
-		for (int y = yBegin; y < yBegin + recLenY; y++) {
-			for (int x = xBegin; x < xBegin + recLenX; x++) {
-				field[x - xBegin][y - yBegin] = observations[x][y];
-			}
-		}
-		return field;
-	}
-	private int[][] getLefttReceptiveField(){
-		int[][] field = new int[recLenX][recLenY];
-		int xBegin = midX;
-		int yBegin = midY - (int) Math.ceil((float) recLenY / 2);
-		for (int y = yBegin; y < yBegin + recLenY; y++) {
-			for (int x = xBegin-recLenX+1; x <= xBegin; x++) {
-				field[x - xBegin+recLenX-1][y - yBegin] = observations[x][y];
-			}
-		}
-		mirrorReceptiveField(field);
-		return field;
-	}
-	
-	void swapArrayElems(int[][] arr, int i, int j, int y){
-		int tmp = arr[i][y];
-		arr[i][y] = arr[j][y];
-		arr[j][y] = tmp;
-	}
-	
-	void mirrorReceptiveField(int[][] field){
-		for(int y = 0; y < recLenY; y++){
-			int swapDist = recLenX-1;
-			for(int x= 0; x < recLenX/2; x++){
-				swapArrayElems(field,x,x+swapDist,y);
-				swapDist-=2;
-			}
-		}
-	}
+	STATE isObstacleAhead() {
+		for (int i = 0; i < obstacleValues.size(); i++)
+			if (recField.tunneledFieldContains(field, obstacleValues.get(i)))
+				return STATE.SUCCESS;
 
-	void setReceptiveField(MarioForwardModel model) {// obersvations middle:
-														// (16,8)
-		observations = model.getMarioCompleteObservation(0, 0);
-		System.out.println(observations.length + " length "+ observations[0].length);
-		
-		if(model.isFacingRight())
-			receptiveField = getRightReceptiveField();
-		else
-			receptiveField = getLefttReceptiveField();
-	}
-	
-	void printReceptiveField(){
-		System.out.println("-------------------");
-		for(int y = 0; y < 6; y++){
-			for(int x = 0; x < 5; x++){
-				System.out.print(receptiveField[x][y] + ",");
-			}
-			System.out.println();
-		}
-		System.out.println("-------------------");
+		return STATE.FAILURE;
 	}
 
 	@Override
 	public boolean[] getActions(MarioForwardModel model, MarioTimer timer) {
-
-		setReceptiveField(model);
-		printReceptiveField();
-		// int[] pos = getMarioBoundedPos(model);
-
-		// System.out.println(model.isFacingRight());
-
-		// System.out.println(observ[0][0] + " x:"+ pos[0] + " y:" + pos[1] + "
-		// _ " + observ.length + "_"+observ[0].length);
-		// for(int i = 0 ; i < 16; i++){
-		// System.out.println(" "+ i + ":" + mapBoolToInt((observ[0][i] ==
-		// model.OBS_BRICK)));
-		// }
-		// System.out.println("-----------------------------------");
-
-		// System.out.println("x: " + pos[0] + " y: " + pos[1] + " obs: " +
-		// (observ[17][8]));
-
+		this.model = model;
+		field = recField.getReceptiveField(model);
+		recField.printReceptiveField(field);
+		System.out.println("OBSTACLE: " + isObstacleAhead());
 		return actions;
 	}
 
