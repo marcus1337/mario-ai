28550e386a71609e1205ab039a32edd5d5ac81a6
marcus1337 <blurgas@hotmail.com>
2020-05-29 13:35:05 +0200

    refactor

--- visuals/SampleTreeFactory.java
+++ visuals/SampleTreeFactory.java
@@ -7,28 +7,40 @@ import java.util.Stack;
 
 import org.abego.treelayout.TreeForTreeLayout;
 import org.abego.treelayout.util.DefaultTreeForTreeLayout;
-
-import agents.BT.BT.Interior;
 import agents.BT.BT.Node;
 import agents.BT.BT.Tree;
 
 public class SampleTreeFactory {
 
-	public static int interiorWidth = 20;
-
 	public static TreeForTreeLayout<Node> createSampleTree(Tree tree) {
-
 		Stack<Node> nodeStack = new Stack<Node>();
 		Queue<TextInBox> textBoxStack = new LinkedList<TextInBox>();
-		int counter = 0;
-		int numParents = 0;
-		
+		addNodesToQueue(tree, nodeStack, textBoxStack);
+		TextInBox rootBox = textBoxStack.peek();
+		DefaultTreeForTreeLayout<Node> treeLayout = new DefaultTreeForTreeLayout<Node>(rootBox.node);
+		connectVisualGraphNodes(textBoxStack, treeLayout);
+		return treeLayout;
+	}
+
+	public static void connectVisualGraphNodes(Queue<TextInBox> textBoxStack,
+			DefaultTreeForTreeLayout<Node> treeLayout) {
+		while (!textBoxStack.isEmpty()) {
+			TextInBox txtBox1 = textBoxStack.poll();
+			if (txtBox1.node.isParent()) {
+				ArrayList<Node> children = txtBox1.node.getChildren();
+				for (Node n : children) {
+					TextInBox txtBox2 = textBoxStack.poll();
+					treeLayout.addChild(txtBox1.node, txtBox2.node);
+				}
+			}
+		}
+	}
+
+	public static void addNodesToQueue(Tree tree, Stack<Node> nodeStack, Queue<TextInBox> textBoxStack) {
 		nodeStack.push(tree.root);
 		while (!nodeStack.empty()) {
 			Node node = nodeStack.pop();
-			counter++;
 			if (node.isParent()) {
-				numParents++;
 				TextInBox tmpParentTxtBox = new TextInBox(node);
 				node.width = tmpParentTxtBox.width;
 				node.height = tmpParentTxtBox.height;
@@ -45,24 +57,6 @@ public class SampleTreeFactory {
 				}
 			}
 		}
-		
-		TextInBox rootBox = textBoxStack.peek();
-		DefaultTreeForTreeLayout<Node> treeLayout = new DefaultTreeForTreeLayout<Node>(rootBox.node);
-		
-		
-		while (!textBoxStack.isEmpty()) {
-			TextInBox txtBox1 = textBoxStack.poll();
-			if (txtBox1.node.isParent()) {
-				ArrayList<Node> children = txtBox1.node.getChildren();
-				for (Node n : children) {
-					TextInBox txtBox2 = textBoxStack.poll();
-					treeLayout.addChild(txtBox1.node, txtBox2.node);
-				}
-			}
-		}
-		
-
-		return treeLayout;
 	}
 
 
