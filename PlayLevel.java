3e3ff3b0e0c1b570e10f3e264a1706878587aeb1
marcus1337 <blurgas@hotmail.com>
2020-06-10 15:04:22 +0200

    refactor

--- PlayLevel.java
+++ PlayLevel.java
@@ -16,10 +16,10 @@ public class PlayLevel {
     	int maxGens = 100;
     	
     	NEATTester neatTester = new NEATTester(100, "NEAT_DIST2");
-    	neatTester.evolveNEATsFromScratch(maxGens);
+    	//neatTester.evolveNEATsFromScratch(maxGens);
     	
     	//neatTester.loadAndShowNEATAgent(neatTester.fileNameBT, 5, 5, 21);
-    	//neatTester.loadAndShowEliteNEATAgent(neatTester.eliteFolderName, 0, 21);
+    	neatTester.loadAndShowEliteNEATAgent(neatTester.eliteFolderName, 1357, 21);
     	neatTester.cleanUp();
     	
     	
