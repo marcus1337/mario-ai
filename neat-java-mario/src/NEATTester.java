b1a905a051d0a0dfc4f3e8a0bdfa3248ee2f2634
marcus1337 <blurgas@hotmail.com>
2021-03-26 12:14:24 +0100

    fixes

--- neat-java-mario/src/NEATTester.java
+++ neat-java-mario/src/NEATTester.java
@@ -111,9 +111,9 @@ public class NEATTester {
 		long startTime = System.currentTimeMillis();
 		while (startTime + timeLimit > System.currentTimeMillis()) {
 			numGenerations++;
-			if(numGenerations % 1 == 0)
+			if(numGenerations % 5 == 0)
 				testAndStoreElites();
-			if(numGenerations % 3 == 0)
+			if(numGenerations % 30 == 0)
 				evolver.randomizePopulationViaElites();
 			evolveGeneration();
 			System.out.println("Generations complete: " + Integer.toString(numGenerations));
