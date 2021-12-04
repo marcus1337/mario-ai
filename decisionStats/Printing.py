05c26013276e30853fd6904cd9c198431cef5248
marcus1337 <blurgas@hotmail.com>
2021-12-04 15:38:00 +0100

    refactor

--- decisionStats/Printing.py
+++ decisionStats/Printing.py
@@ -40,21 +40,24 @@ class ResultPrinter:
         result += "|"
         return result
 
-    def printResults(self, counter, title):
+    def firstTableColumn(self, eventType):
+        self.ljustValue = 0
+        row = eventType
+        return self.addLjust(row, 30)
 
+    def addTableContent(self, tableStr, values):
+        tableStr = self.addEventValue(tableStr, values.num_event_occurances)
+        for i in range(0, 6):
+            tableStr = self.addEventValue(tableStr, values.get(i))
+        return tableStr
+
+    def printResults(self, counter, title):
         result = self.getTitle(title) + "\n"
         result += self.tableTop
-
         for key in counter.events:
-            self.ljustValue = 0
-            row = key
-            values = counter.events[key]
-
-            row = self.addLjust(row, 30)
-            num_events = values.num_event_occurances
-            row = self.addEventValue(row, values.num_event_occurances)
-            for i in range(0, 6):
-                row = self.addEventValue(row, values.get(i))
+            row = self.firstTableColumn(key)
+            row = self.addTableContent(row, counter.events[key])
             result += row + "\n"
-        
         print(result)
+
+#num_events = values.num_event_occurances
